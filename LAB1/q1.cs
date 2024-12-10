0dde01bf2467eb13564d2a8ce7e1dd3708d8ab64
Rujan <grimr0833@gmail.com>
2024-12-10 09:37:47 +0545

    her bsdk

---
+++ LAB1/q1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB1
+{
+    
+    internal class Program
+    {
+        class sum
+    {
+        public int x;
+        public int y;
+        public sum(int x, int y)
+        {
+            Console.WriteLine($"sum is {x + y}");
+        }
+    }
+        static void Main(string[] args)
+        {
+            int x = Convert.ToInt32(Console.ReadLine());
+            int y = Convert.ToInt32(Console.ReadLine());
+
+            sum add = new sum(x, y);
+            Console.ReadKey();  
+        }
+    }
+}
---
+++ LAB1/q2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace lab1
+{
+    internal class Program
+    {
+        class Studentq2
+        {
+            public int StudentId { get; set; }
+            public string StudentName { get; set; }
+            public void Studentq(int id, string name)
+            {
+                this.StudentId = id;
+                this.StudentName = name;
+
+                Console.WriteLine($"Student ID: {StudentId}, Student Name: {StudentName}");
+            }
+        }
+
+        static void Main(string[] args) 
+        {
+            Studentq2 stq = new Studentq2();
+            stq.Studentq(101, "Magnus Carlson");
+
+
+            Console.ReadKey();
+        }
+    }
+}
